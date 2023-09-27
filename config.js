83779f99c5f8a7c0a6a048840e52926bcc9c11ff
Rajnish <RajnishAryan12@gmail.com>
2023-09-27 08:49:04 +0530

    database added

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+module.exports = {
+    development: {
+      username: root,
+      password:123456,
+      database:nodejs,
+      host: 'localhost',
+      dialect: 'mysql',
+    },
+  };
+  
