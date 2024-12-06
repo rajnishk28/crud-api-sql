e61b94ec98f40ef6f7aaa5c39c09a82d28c79b33
Rajnish <rajnisharyan12@gmail.com>
2024-12-06 16:04:18 +0530

    winston and logger added

--- app.js
+++ app.js
@@ -4,12 +4,31 @@ const sequelize = require('./config/mySqlDatabase');
 const userRoutes = require('./routes/userRoutes');
 const mongoDatabase = require("./config/mongoDatabase")
 const fileUploadRoutes =require("./routes/file.routes")
+const morgan =require("morgan");
+const {logger} =require("./utils/logger")
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware for parsing JSON requests
 app.use(bodyParser.json());
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 // Apply user routes
 app.use('/users', userRoutes);
